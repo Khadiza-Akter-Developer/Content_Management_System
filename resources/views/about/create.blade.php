6c57ccc146521c0734ad49ec17a220bd9fdf74f9
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-05-11 22:34:05 +0600

    value

--- resources/views/about/create.blade.php
+++ resources/views/about/create.blade.php
@@ -7,7 +7,7 @@
         <div class="col-12 grid-margin stretch-card">
     
           <div class="card">
-    
+     
             <div class="card-body">
                 <div class="d-flex justify-content-end mb-3">
                     <a href="{{ route('about') }}" class="btn btn-gradient-dark btn-sm">Back</a>
@@ -15,30 +15,32 @@
     
               <h4 class="card-title">Create About</h4>
 
+             
               @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
+              <div class="alert alert-danger">
+                  <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                      @endforeach
+                  </ul>
+              </div>
+          @endif
 
               <form class="forms-sample" action="{{ route('about.store')}}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="form-group">
                   <label for="" class="form-label">Title</label>
-                  <input type="text" class="form-control" placeholder="Title" name="title">
+                  <input type="text" class="form-control" placeholder="Title" name="title" value="{{ old('title') }}">
                 </div>
+                
                 <div class="form-group">
                   <label for="">Image</label>
-                  <input type="file" id="image" class="form-control" placeholder="Image" name="image">
+                  <input type="file" id="image" class="form-control" placeholder="Image" name="image" value="{{ old('image') }}">
                 </div>
+
                 <div class="form-group">
                   <label for="">Description</label>
-                  <input type="text" class="form-control" placeholder="Description" name="description">
+                  <input type="text" class="form-control" placeholder="Description" name="description" value="{{ old('description') }}">
                 </div>
                
                 <button type="submit" class="btn btn-gradient-dark btn-sm">Submit</button>
