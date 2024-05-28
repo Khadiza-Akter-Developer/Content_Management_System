1b9e5190748edf03b6b954f8f37d3f16b0319e35
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-05-28 15:37:15 +0600

    website index

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -1,15 +1,18 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
-use App\Models\Blog;
+use App\Models\Blog; 
 use Illuminate\Http\Request;
 
 class WebsiteController extends Controller
 {
     public function home()
     {
-        $blog = Blog::all();
-        return view('website.blog.index', compact('blog'));
+        $blogs = Blog::all();
+        
+        return view('website.blog.index', compact('blogs'));
     }
 }
+
