7f98f4a8953a70ce6a1a7b1b6be3a3107feb6574
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-05-28 16:25:45 +0600

    description

--- resources/views/website/blog/index.blade.php
+++ resources/views/website/blog/index.blade.php
@@ -40,19 +40,8 @@
                                 </ul>
                             </div>
                             <div class="post-content">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit vitae placeat ad
-                                    architecto
-                                    nostrum
-                                    asperiores vel aperiam, veniam eum nulla. Maxime cum magnam, adipisci architecto
-                                    quibusdam
-                                    cumque veniam
-                                    fugiat quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio vitae ab
-                                    doloremque accusamus
-                                    sit, eos dolorum officiis a perspiciatis aliquid. Lorem ipsum dolor sit amet,
-                                    consectetur
-                                    adipisicing
-                                    elit. Quod, facere. </p>
-                                <a href="blog-single.html" class="btn btn-main">Read More</a>
+                                <p>{{ Str::limit($it->description, 100) }} </p>
+                                <a href="#" class="btn btn-main">Read More</a>
                             </div>
                         </div>
                 </div>
