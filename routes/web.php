2c18b42ac148efb51029a3d775a54bee10ca4dce
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-06-02 21:26:13 +0600

    wweb

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::get('/', function () {
-  //  auth()->logout();
-    return 'home';
+  // auth()->logout();
+  return 'home';
 });
 
 //slider route curd
