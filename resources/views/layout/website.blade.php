f5d9bd9b89201459bce1bf3823ee61acaa9bafd9
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-06-03 23:38:25 +0600

    cms

--- resources/views/layout/website.blade.php
+++ resources/views/layout/website.blade.php
@@ -14,7 +14,7 @@
     <!-- Basic Page Needs
   ================================================== -->
     <meta charset="utf-8">
-    <title>Airspace</title>
+    <title>CMS</title>
 
     <!-- Mobile Specific Metas
   ================================================== -->
