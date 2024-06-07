28a5474aef19ca5c042bdca9a512a4c6be395427
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-06-07 23:02:05 +0600

    t

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,6 +27,7 @@
         <!-- Small boxes (Stat box) -->
         <div class="row">
           <div class="col-lg-3 col-6">
+            <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
                 <h3>150</h3>
